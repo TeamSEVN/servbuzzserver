474f11e1b9277055d13459b400cc72cb2d5188da
VinnyWr <43010025+VinnyWr@users.noreply.github.com>
2020-12-08 12:33:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ require("./models/Buzz");
 app.use(require("./routes"));
 
 //store mongoDB Atlas url
-let mongoDB = "mongodb+srv://user:9TSa4Ety5SJpUkn@servbuzzcluster.embtt.mongodb.net/vinny?retryWrites=true&w=majority
+let mongoDB = "mongodb+srv://user:9TSa4Ety5SJpUkn@servbuzzcluster.embtt.mongodb.net/vinny?retryWrites=true&w=majority"
 //old connection string======let mongoDB = "mongodb://user:password1@ds115753.mlab.com:15753/vinny";
 
 //connect to the database
